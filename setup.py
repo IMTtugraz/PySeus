a88e5df59e205ab44a1a924d0d5ee6d12e0773e4
calmer <christoph.almer@gmail.com>
2019-03-27 12:36:49 +0100

    Setup basic project structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='pyseus',
+    version='0.1',
+    description='',
+    url='http://github.com/calmer/pyseus',
+    author='Christoph Almer',
+    author_email='christoph.almer@gmail.com',
+    license='GNU',
+    packages=['pyseus'],
+    install_requires=[
+        '',
+    ],
+    zip_safe=False
+)
