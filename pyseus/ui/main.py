5f90debb98eb5cd57197ac73256e7542d95151a7
calmer <christoph.almer@gmail.com>
2019-07-14 14:40:34 +0200

    Basic Stylesheet

--- pyseus/ui/main.py
+++ pyseus/ui/main.py
@@ -2,13 +2,18 @@ import sys
 import webbrowser
 
 from PySide2 import QtWidgets, QtCore
-from PySide2.QtWidgets import QApplication, QMainWindow, QAction, QLabel
+from PySide2.QtWidgets import QApplication, QMainWindow, QAction, \
+    QLabel, QScrollArea
+from PySide2.QtGui import QImage, QPixmap, QPalette
 
 class MainWindow(QMainWindow):
     def __init__(self, app):
         QMainWindow.__init__(self)
         self.setWindowTitle("PySEUS")
+        self.setup_stylesheet(app)
 
+        # Menu Bar
+        self.menu = self.menuBar()
         self.setup_menu()
 
         # Status Bar
@@ -17,18 +22,16 @@ class MainWindow(QMainWindow):
         
         # Image View & Scroll Area
         self.view = QLabel()
-        # self.scrollArea = QScrollArea()
-        # self.scrollArea.setWidget(imageLabel)
+        self.scrollArea = QScrollArea()
+        self.scrollArea.setWidget(self.view)
 
-        # self.setCentralWidget(self.scrollArea)
+        self.setCentralWidget(self.scrollArea)
 
         # Window dimensions
         geometry = app.desktop().availableGeometry(self)
-        self.setFixedSize(geometry.width() * 0.8, geometry.height() * 0.7)
+        self.setFixedSize(geometry.width() * 0.5, geometry.height() * 0.6)
 
     def setup_menu(self):
-        self.menu = self.menuBar()
-
         # File Menu
         self.file_menu = self.menu.addMenu("File")
 
@@ -69,6 +72,20 @@ class MainWindow(QMainWindow):
         self.about_menu.addAction(about_action)
 
         # End About Menu
+
+    def setup_stylesheet(self, app):
+        app.setStyleSheet("""
+QMenuBar { background: #111; color: #ddd; }
+QMenuBar::item { padding: 5px 10px 5px 10px; }
+QMenuBar::item:selected { background: #222; }
+QMenu { background: #222; color: #eee; padding: 0px; }
+QMenu::item { padding: 5px 10px 5px 10px; }
+QMenu::item:selected { background: #333; }
+QScrollArea { background: #111; border: none; }
+QLabel { background: #111; }
+QStatusBar { background: #222; color: #eee; }
+"""
+        )
 
     def _action_exit(self):
         sys.exit()
