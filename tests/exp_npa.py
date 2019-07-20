faef84dbcbb29e22f39494a5373cd3b99986860f
calmer <christoph.almer@gmail.com>
2019-07-20 09:49:14 +0200

    Tests Clean Up

--- tests/exp_npa.py
+++
@@ -1,7 +0,0 @@
-import numpy
-import imageio
-
-if __name__ == '__main__':
-    pic = imageio.imread('npa.jpg')
-    grayscale = (pic[:, :, 0]+pic[:, :, 1]+pic[:, :, 2])
-    numpy.savetxt("npa.txt", grayscale, fmt='%1.0f')
--- tests/test_call.py
+++
@@ -1,8 +0,0 @@
-import numpy
-
-import context
-from context import pyseus
-
-if __name__ == "__main__":
-    npa = numpy.loadtxt("npa.txt", numpy.uint8)
-    pyseus.show(npa.data)
