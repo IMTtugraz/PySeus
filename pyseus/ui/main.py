9ee58893655a7d23d5b7fa31c7ab143fbc61cdb6
calmer <christoph.almer@gmail.com>
2019-07-20 09:48:47 +0200

    Viewer Functions

--- pyseus/ui/main.py
+++ pyseus/ui/main.py
@@ -23,11 +23,12 @@ class MainWindow(QMainWindow):
         # Image View & Scroll Area
         self.view = QLabel()
         self.view.setScaledContents(True)
+        self.zoom_factor = 1
 
-        image = QImage("./tests/test.jpg")
-        self.view.setPixmap(QPixmap.fromImage(image))
         self.scrollArea = QScrollArea()
         self.scrollArea.setWidget(self.view)
+        # Reset Scroll event Handler
+        self.scrollArea.wheelEvent = lambda e: False
 
         self.setCentralWidget(self.scrollArea)
 
@@ -35,63 +36,31 @@ class MainWindow(QMainWindow):
         geometry = app.desktop().availableGeometry(self)
         self.setFixedSize(geometry.width() * 0.5, geometry.height() * 0.6)
 
+    def add_menu_item(self, menu, title, callback, shortcut = ""):
+        action = QAction(title, self)
+        if(shortcut != ""):
+            action.setShortcut(shortcut)
+        action.triggered.connect(callback)
+        menu.addAction(action)
+        return action
+
     def setup_menu(self):
         # File Menu
         self.file_menu = self.menu.addMenu("File")
 
-        # Load Action
-        load_action = QAction("Load", self)
-        load_action.setShortcut("Ctrl+O")
-        load_action.triggered.connect(self._action_load)
-
-        # Exit Action
-        exit_action = QAction("Exit", self)
-        exit_action.setShortcut("Ctrl+Q")
-        exit_action.triggered.connect(self._action_exit)
-
-        self.file_menu.addAction(load_action)
-        # self.file_menu.addSeparator()
-        self.file_menu.addAction(exit_action)
-
-        # End File Menu
+        self.add_menu_item(self.file_menu, "Load", self._action_load, "Ctrl+O")
+        self.add_menu_item(self.file_menu, "Exit", self._action_exit, "Ctrl+Q")
         
         # View Menu
         self.view_menu = self.menu.addMenu("View")
 
-        # Zoom In Action
-        zoom_in_action = QAction("Zoom in", self)
-        zoom_in_action.setShortcut("+")
-        zoom_in_action.triggered.connect(self._action_zoom_in)
-        
-        # Zoom Out Action
-        zoom_out_action = QAction("Zoom out", self)
-        zoom_out_action.setShortcut("-")
-        zoom_out_action.triggered.connect(self._action_zoom_out)
-        
-        # Fit Action
-        zoom_fit_action = QAction("Fit", self)
-        zoom_fit_action.setShortcut("#")
-        zoom_fit_action.triggered.connect(self._action_zoom_fit)
-        
-        # Reset Action
-        reset_action = QAction("Reset", self)
-        reset_action.setShortcut("0")
-        reset_action.triggered.connect(self._action_zoom_reset)
+        self.add_menu_item(self.view_menu, "Zoom in", self._action_zoom_in, "+")
+        self.add_menu_item(self.view_menu, "Zoom out", self._action_zoom_out, "-")
+        self.add_menu_item(self.view_menu, "Fit", self._action_zoom_fit, "#")
+        self.add_menu_item(self.view_menu, "Reset", self._action_zoom_reset, "0")
 
-        self.view_menu.addAction(zoom_in_action)
-        self.view_menu.addAction(zoom_out_action)
-        self.view_menu.addAction(zoom_fit_action)
-        self.view_menu.addAction(reset_action)
-
-        # End View Menu
-
-        # About Action
-        about_action = QAction("About", self)
-        about_action.triggered.connect(self._action_about)
-
-        self.menu.addAction(about_action)
-
-        # End About Menu
+        # About Menu
+        self.add_menu_item(self.menu, "About", self._action_about)
 
     def setup_stylesheet(self, app):
         app.setStyleSheet("""
@@ -104,42 +73,42 @@ QMenu::item:selected { background: #333; }
 
 QScrollArea { background: #111; border: none; }
 QScrollBar:horizontal {
-  background: transparent;
-  height: 8px;
-  margin: 2px 10px 2px 10px;
+    background: transparent;
+    height: 8px;
+    margin: 2px 10px 2px 10px;
 }
 QScrollBar::handle:horizontal {
-  background-color: #bbb;
-  min-width: 12px;
-  border-radius: 2px;
+    background-color: #bbb;
+    min-width: 12px;
+    border-radius: 2px;
 }
 QScrollBar::handle:horizontal:hover {
-  background-color: #eee;
+    background-color: #eee;
 }
 
 QScrollBar:vertical {
-  background: transparent;
-  width: 8px;
-  margin: 10px 2px 10px 2px;
+    background: transparent;
+    width: 8px;
+    margin: 10px 2px 10px 2px;
 }
 QScrollBar::handle:vertical {
-  background-color: #bbb;
-  min-height: 12px;
-  border-radius: 2px;
+    background-color: #bbb;
+    min-height: 12px;
+    border-radius: 2px;
 }
 QScrollBar::handle:vertical:hover {
-  background-color: #eee;
+    background-color: #eee;
 }
 
 QScrollBar::add-line:horizontal, QScrollBar::sub-line:horizontal {
-  width: 0px;
+    width: 0px;
 }
 QScrollBar::add-line:vertical,QScrollBar::sub-line:vertical {
-  height: 0px;
+    height: 0px;
 }
 QScrollBar::add-page:horizontal, QScrollBar::sub-page:horizontal,
 QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical {
-  background: none;
+    background: none;
 }
 
 QLabel { background: #111; }
@@ -155,20 +124,73 @@ QStatusBar { background: #111; color: #eee; }
         print("load")
     
     def _action_zoom_in(self):
-        print("+")
+        self.zoom_factor = self.zoom_factor * 1.25
+        self.view.resize(self.zoom_factor * self.view.pixmap().size())
         
     def _action_zoom_out(self):
-        print("-")
+        self.zoom_factor = self.zoom_factor * 0.8
+        self.view.resize(self.zoom_factor * self.view.pixmap().size())
         
     def _action_zoom_fit(self):
-        print("#")
+        image = self.view.pixmap().size()
+        viewport = self.scrollArea.size()
+        v_zoom = viewport.height() / image.height()
+        h_zoom = viewport.width() / image.width()
+        self.zoom_factor = min(v_zoom, h_zoom)*0.99
+        self.view.resize(self.zoom_factor * self.view.pixmap().size())
     
     def _action_zoom_reset(self):
-        print("0")
+        self.zoom_factor = 1
+        self.view.resize(self.view.pixmap().size())
     
     def _action_about(self):
         webbrowser.open("https://github.com/calmer/PySEUS", new=0, 
             autoraise=True)
 
-    def view(img):
+    def mousePressEvent(self, event):
+        self.last_position = event.pos()
+        if(event.buttons() == QtCore.Qt.RightButton):
+            self.mouse_move_mode = 3 # pan
+        elif(event.buttons() == QtCore.Qt.MiddleButton):
+            self.mouse_move_mode = 2 # window
+            print("window")
+        elif(event.buttons() == QtCore.Qt.LeftButton):
+            self.mouse_move_mode = 1 # mark roi
+            print("mark roi")
+        else:
+            self.mouse_move_mode = 0 # nothing
+
+    def mouseReleaseEvent(self, event):
+        self.last_position = None
+        self.mouse_move_mode = 0
+
+    def mouseMoveEvent(self, event):
+        if(self.mouse_move_mode == 3): # pan
+            vertical = self.scrollArea.verticalScrollBar().value() \
+                + self.last_position.y() - event.pos().y()
+            horizontal = self.scrollArea.horizontalScrollBar().value() \
+                + self.last_position.x() - event.pos().x()
+
+            self.scrollArea.verticalScrollBar().setValue(vertical)
+            self.scrollArea.horizontalScrollBar().setValue(horizontal)
+            self.last_position = event.pos()
+    
+    def wheelEvent(self, event):
+        change = 0.8 if event.delta() < 0 else 1.25
+        if(self.zoom_factor * change > 5 or self.zoom_factor * change < 0.1): return
+            
+        self.zoom_factor = self.zoom_factor * change
+        self.view.resize(self.zoom_factor * self.view.pixmap().size())
+
+        self.scrollArea.verticalScrollBar().setValue(int(change * \
+            self.scrollArea.verticalScrollBar().value() + \
+            ((change - 1) * self.scrollArea.verticalScrollBar().pageStep()/2)))
+        self.scrollArea.horizontalScrollBar().setValue(int(change * \
+            self.scrollArea.horizontalScrollBar().value() + \
+            ((change - 1) * self.scrollArea.horizontalScrollBar().pageStep()/2)))
+
+    def view_data(self, image):
         self.view.setPixmap(QPixmap.fromImage(image))
+        self._action_zoom_reset()
+
+# @TODO Refactor: setZoom(factor)
