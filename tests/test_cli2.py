f015f5c513a369eee230b97c5556231428071fc2
calmer <christoph.almer@gmail.com>
2019-07-24 14:10:49 +0200

    CLI (Piping) Test

---
+++ tests/test_cli2.py
@@ -0,0 +1,12 @@
+from sys import stdin
+
+from context import pyseus
+
+import ptvsd
+
+# VSC remote debugging
+ptvsd.enable_attach(address=('localhost', 5678), redirect_output=True)
+ptvsd.wait_for_attach()
+breakpoint()
+
+pyseus._console_entry()
