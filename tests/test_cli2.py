5d62d9843095034a64e6a7c53c3f72c1aef0e1eb
calmer <christoph.almer@gmail.com>
2019-08-04 20:36:06 +0200

    Cleaned up test files

--- tests/test_cli2.py
+++
@@ -1,9 +0,0 @@
-import ptvsd
-
-from context import pyseus
-
-# VSC remote debugging
-ptvsd.enable_attach(address=('localhost', 5678), redirect_output=True)
-ptvsd.wait_for_attach()
-
-pyseus._console_entry()
