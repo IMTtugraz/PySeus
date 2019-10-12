973ee12d0163c398c1a7529b40709fa0c047e5a1
calmer <christoph.almer@gmail.com>
2019-10-12 19:21:52 +0200

    rtd setup

--- pyseus/ui/__init__.py
+++ pyseus/ui/__init__.py
@@ -2,6 +2,7 @@
 
 from .main import MainWindow  # noqa: F401
 from .view import ViewWidget  # noqa: F401
+from .info import InfoWidget  # noqa: F401
 from .console import ConsoleWidget  # noqa: F401
 from .thumbs import ThumbsWidget  # noqa: F401
 from .meta import MetaWidget  # noqa: F401
