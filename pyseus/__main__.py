46cb8a6e6da6be172d4fb77052b2d3af479e779f
Maier, Oliver <oliver.maier@tugraz.at>
2020-09-17 11:29:53 +0200

    Changes list to tuple in isinstance check

--- pyseus/__main__.py
+++ pyseus/__main__.py
@@ -16,14 +16,13 @@ def load(arg=None):
     """Start Pyseus and load *arg* (can be a path or data array)."""
 
     app = PySeus()
-
     if len(argv) > 1 and arg is None:
         arg = argv[1]
 
     if isinstance(arg, str):
         app.load_file(arg)
 
-    elif isinstance(arg, [numpy.ndarray, list]):
+    elif isinstance(arg, (numpy.ndarray, list)):
         app.load_data(arg)
 
     return app.show()
