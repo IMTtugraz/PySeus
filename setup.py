600fe92994e4b169791cd27e64030a9353bba3cd
Maier, Oliver <oliver.maier@tugraz.at>
2020-09-23 10:50:07 +0200

    Moved to headless opencv due to qt5 library conflicts with pyside2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=[
         'PySide2',
         'numpy',
-        'opencv-python',
+        'opencv-python-headless',
         'h5py',
         'pydicom',
         'nibabel',
