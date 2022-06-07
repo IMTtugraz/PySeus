5ed6a43a8e529f265fa43367d5d74b078dd8c1aa
impergator <impergator@outlook.de>
2022-06-07 20:50:57 +0200

    Updated some core files to make the denoise and reconstruction algorithms usable

--- pyseus/__main__.py
+++ pyseus/__main__.py
@@ -8,20 +8,23 @@ Methods
 
 from sys import argv
 import numpy
+from .settings import DataType
 
 from .core import PySeus
 
 
-def load(arg=None):
+def load(arg=None, data_type=DataType.IMAGE):
     """Start Pyseus and load *arg* (can be a path or data array)."""
 
     app = PySeus()
     if len(argv) > 1 and arg is None:
         arg = argv[1]
+    if len(argv) > 2:
+        data_type = argv[2]
     if isinstance(arg, str):
-        app.load_file(arg)
+        app.load_file(arg, data_type)
     elif isinstance(arg, (numpy.ndarray, list)):
-        app.load_data(arg)
+        app.load_data(arg, data_type)
 
     return app.show()
 
--- pyseus/settings.py
+++ pyseus/settings.py
@@ -2,7 +2,37 @@
 
 import os
 from configparser import ConfigParser
+from enum import IntEnum
 
 settings = ConfigParser()  # pylint: disable=C0103
 settings.read(os.path.abspath(os.path.join(
     os.path.dirname(__file__), "./settings.ini")))
+
+class DataType(IntEnum):
+    """ Enumeration definition for data type of loaded file"""
+
+    IMAGE = 0
+    KSPACE = 1
+    PHASE = 2
+
+class ProcessType(IntEnum):
+    """ Enumeration definition for processing type"""
+
+    DENOISING = 0
+    RECONSTRUCTION = 1
+
+class ProcessSelDataType(IntEnum):
+    """ Enumeration definition for the selection of the processed dataset"""
+
+    SLICE_2D = 0
+    WHOLE_SCAN_2D = 1
+    WHOLE_SCAN_3D = 2
+
+
+class ProcessRegType(IntEnum):
+    """ Enumeration definition for regularisation type of processed dataset"""
+
+    TV_L1 = 0 
+    HUB_L2 = 1
+    TV_L2 = 2
+    TGV2_L2 = 3
